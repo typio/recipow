69eda590a2e405199dc971e940c9eafaf73abc4a
Thomas Huber <me@tohuber.com>
2022-06-02 19:42:35 -0700

    switch from node to netlify

--- src/lib/components/header/Header.svelte
+++ src/lib/components/header/Header.svelte
@@ -88,6 +88,7 @@
 			<li style="display: {loggedIn ? 'block' : 'none'}">
 				<button on:click={logOut}>Log Out</button>
 			</li>
+
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,11 +1,23 @@
 import Redis from "ioredis"
+import { MongoClient } from "mongodb"
 
-const { VITE_REDIS_PORT, VITE_REDIS_HOST, VITE_REDIS_PASSWORD } = import.meta.env
+const { VITE_REDIS_PORT, VITE_REDIS_HOST, VITE_REDIS_PASSWORD,
+    VITE_MONGO_PASSWORD,
+    VITE_MONGO_USERNAME,
+    VITE_MONGO_CLUSTER } = import.meta.env
 
 const redis = new Redis({
-    port: VITE_REDIS_PORT,
-    host: VITE_REDIS_HOST,
-    password: VITE_REDIS_PASSWORD,
+    port: VITE_REDIS_PORT as unknown as number,
+    host: VITE_REDIS_HOST as string,
+    password: VITE_REDIS_PASSWORD as string,
+})
+
+const uri = `mongodb+srv://${VITE_MONGO_USERNAME}:${VITE_MONGO_PASSWORD}@${VITE_MONGO_CLUSTER}`
+const client = new MongoClient(uri)
+client.connect(err => {
+    const collection = client.db("test").collection("devices")
+    // perform actions on the collection object
+    client.close()
 })
 
 export default redis
