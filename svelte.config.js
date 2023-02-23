bea16fe53eeaca69ff9560ae97b05adabae6be10
Thomas Huber <me@tohuber.com>
2023-02-22 23:55:32 -0700

    Changed config for vercel :small_red_triangle:

--- svelte.config.js
+++ svelte.config.js
@@ -2,31 +2,13 @@ import vercel from '@sveltejs/adapter-vercel'
 import preprocess from 'svelte-preprocess'
 
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess({
-		postcss: true
-	}),
-	kit: {
-		// default options are shown
-		adapter: vercel({
-			// if true, will deploy the app using edge functions
-			// (https://vercel.com/docs/concepts/functions/edge-functions)
-			// rather than serverless functions
-			edge: true,
-
-			// an array of dependencies that esbuild should treat
-			// as external when bundling functions
-			external: [],
-
-			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app
-			split: false
-		}),
-
-		// prerender: {
-		// 	enabled: false
-		// }
-	}
+    preprocess: preprocess({
+        postcss: true
+    }),
+    kit: {
+        adapter: vercel({
+            runtime: 'edge'
+        })
+    }
 }
 export default config
